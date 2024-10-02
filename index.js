e5e7abc8f27bc7ae28d6a32ba2914626a50d47cb
QPDoyle <qpdoyle13@gmail.com>
2024-10-02 12:15:07 -0400

    index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+/*
+Modeling a team of playersfrom fifa 
+Team:
+    Team Name:
+    Number of players:
+    Rating/Overall:
+        Player
+            name:
+            age:
+            position:
+            team:
+            nationality:
+            rating:
+        Events:
+            Goal Scored:
+            If goalie:
+                Goal Saved:
+                Goal Conceded
+    Goals+1
+    Goals-1
+*/
